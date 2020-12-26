0f92bd982ae7685c6b105003dfeea42e71c245b8
chunghwanyoon <chunghwan@redrocket.co.kr>
2020-12-26 23:02:13 +0900

    add cls-hook init, swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { initializeTransactionalContext } from 'typeorm-transactional-cls-hooked';
+import { SwaggerModule } from '@nestjs/swagger';
+import { BaseAPISpecifications } from './api/Base';
+import 'dotenv/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  createConnection('default')
+    .then(async (connection) => {
+      /* init cls-hooked */
+      initializeTransactionalContext();
+
+      /* app bootstrap */
+      const app = await NestFactory.create(AppModule);
+
+      /* mount Swagger API Documentation */
+      const documentOptions = new BaseAPISpecifications().initializeOptions();
+      const document = SwaggerModule.createDocument(app, documentOptions);
+      SwaggerModule.setup('api/docs', app, document);
+
+      await app.listen(process.env.APP_PORT);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 bootstrap();
